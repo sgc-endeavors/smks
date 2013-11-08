217beb6908bf97b04631c58755b561238a9a6053
Seth Geyer <seth.geyer@gmail.com>
2013-11-08 11:48:56 -0700

    Added feature for checking for "app name" w/in h1 tag.

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
  describe "Registration Page" do
+    subject { page }
+   
    it "includes a page title" do
+    
      visit new_user_path#"/users/new"
-     expect(page).to have_content("smks")
-     
+     #expect(page).to have_content("smks")
+     #has_content?("smks")
+     #page.should have_content("sasd")
+     within("h1") do
+      should have_content("smks")
+     end
+     current_path.should == "/users/new"
    end
  end
