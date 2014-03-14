c5baa1ff654ea2a256fbc8948cbbf0ad96a72f8a
Seth Geyer <seth.geyer@gmail.com>
2014-03-14 15:46:13 -0600

    added cong.assets_initialize_on_precompile = false to address the git push error on heroku

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,13 @@ module App
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    ######If you are deploying on Heroku with Rails 3.2 only, you may want to set:
+        config.assets.initialize_on_precompile = false
+     #####On config/application.rb forcing your application to not access the DB
+     #####or load models when precompiling your assets.
+
+
+    
   end
 end
